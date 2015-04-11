958528dfb0073b8a2e30b1d48f0dc3183a34e5cd
pizzaboi <tophamhatt.crazy@gmail.com>
2015-04-12 02:06:27 +0900

    changed comments

--- set03/q24_tokenize.py
+++ set03/q24_tokenize.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
+improved version of q23_tokenize.py
 USAGE: cat medline.txt.sent | python q24_tokenize.py
 """
 
@@ -9,11 +10,11 @@ import re
 import sys
 
 def tokenize():
-	stopwords = re.compile(r'[^a-z^A-Z^0-9]+$')
+	stopwords = re.compile(r'[^a-z^A-Z^0-9]+$') #added
 	for line in sys.stdin:
 		line = line.strip('\n')
 		for token in line.split(' '):
-			print re.sub(stopwords, repl, token)
+			print re.sub(stopwords, repl, token) #changed
 		print
 
 def repl(obj):
