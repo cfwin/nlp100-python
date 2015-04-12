912f83b3c4ea71513889a80899923db238a7a710
pizzaboi <tophamhatt.crazy@gmail.com>
2015-04-13 03:49:27 +0900

    added q34_lacks.py

---
+++ set04/q34_lacks.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+derivated from q33_marshal.py
+USAGE: cat medline.txt.sent.tok.stem | python set04/q34_lacks.py
+"""
+
+import argparse
+import marshal
+import sys
+
+def reader(column, dict):
+	D = marshal.load(open(dict))
+	for line in sys.stdin:
+		line = line.strip('\n')
+		token = line.split('\t')[column - 1]
+		if D.has_key(token):
+			pass
+			#entries = []
+			#for item in D[token]:
+			#	entries.append('(%s)' % ','.join(item))
+			#print '%s\t[%s]' % (line, ', '.join(entries))
+		else:
+			print line
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-c', '--column', type=int, default="2")
+	parser.add_argument('-d', '--dict', type=str, default="inflection.table.txt.msl")
+	args = parser.parse_args()
+	return args
+
+if __name__ == "__main__":
+	args = parse_args()
+	reader(args.column, args.dict)
