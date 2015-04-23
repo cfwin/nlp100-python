4caf3d672126a040a275972a2a6f2016bedb9d00
pizzaboi <tophamhatt.crazy@gmail.com>
2015-04-23 14:48:34 +0900

    revised q81_mongo.py

--- set09/q81_mongo.py
+++ set09/q81_mongo.py
@@ -1,3 +1,31 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+USAGE: gzcat tweets100.tsv.gz | python q81_mongo.py
+
+$ mongo
+> use [dbname]
+> db.stats()
+"""
+
+import pymongo
+import sys
+
+db_name = 'nlp100_pizzaboi'
+
+def tweet2dict(line):
+	names = ('url','data','user','name','body','rt','rep','qt','rtf','repf','qtf')
+	line = line.strip('\n').split('\t')
+	if len(line) == 11:
+		D = dict(zip(line, names))
+	return D
+
+if __name__ == '__main__':
+	conn = pymongo.Connection()
+	db = conn[db_name]
+	col = db['tweets']
+
+	for line in sys.stdin:
+		col.insert( tweet2dict(line) )
+
